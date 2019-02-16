b0199f16facf36089d955c8681046df46199255b
Micah Monfrini <ffrock905@gmail.com>
2019-02-16 16:41:29 -0600

    add fetch request for user

--- github.js
+++ github.js
@@ -5,6 +5,6 @@ class GitHub {
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
   }
 }
