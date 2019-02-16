c5e13cf4286cdbb1a01ce042793a5a369b45d850
Micah Monfrini <ffrock905@gmail.com>
2019-02-16 16:40:14 -0600

    add fetch request for user

--- github.js
+++ github.js
@@ -3,4 +3,8 @@ class GitHub {
     this.client_id = 'e0dacd65c29015d9b3c3';
     this.client_secret = '61aeb02666c581af8961b02c31d8a0591697af33';
   }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+  }
 }
