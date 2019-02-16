4fa2f3eaaabf5513d3f3ca9e6abd656b1cce3e98
Micah Monfrini <ffrock905@gmail.com>
2019-02-16 16:28:57 -0600

    add event listener for search input

---
+++ app.js
@@ -0,0 +1,12 @@
+// Search input
+const searchUser = document.getElementById('search-user');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  userText = e.target.value;
+
+  if(userText !== '') {
+    
+  }
+})
