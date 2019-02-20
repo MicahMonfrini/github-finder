5370d01fc300936a2581e9141345a17408299e5e
Micah Monfrini <ffrock905@gmail.com>
2019-02-19 21:04:12 -0600

    add html

--- ui.js
+++ ui.js
@@ -4,6 +4,12 @@ class UI {
   }
 
   showProfile(user) {
-
+    this.profile.innerHTML = `
+      <div class="card card-body mb-3">
+        <div class="row">
+          <h1>IT WORKS</h1>
+        </div>
+      </div>
+    `
   }
 }
