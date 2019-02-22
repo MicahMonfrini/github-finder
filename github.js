f06e257e6715aed8d53cf55dae5e3475e8ce6a61
Micah Monfrini <ffrock905@gmail.com>
2019-02-21 20:23:38 -0600

    add query strings for repos

--- github.js
+++ github.js
@@ -2,12 +2,14 @@ class GitHub {
   constructor() {
     this.client_id = 'e0dacd65c29015d9b3c3';
     this.client_secret = '61aeb02666c581af8961b02c31d8a0591697af33';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
 
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret${this.client_secret}`);
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret${this.client_secret}`);
 
     const profile = await profileResponse.json();
 
