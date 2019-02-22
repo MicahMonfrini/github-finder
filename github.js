55e28f5f4d124d5b0dadeeb4a619ca579f6d7826
Micah Monfrini <ffrock905@gmail.com>
2019-02-21 20:21:16 -0600

    add repoResponse fetch

--- github.js
+++ github.js
@@ -7,6 +7,8 @@ class GitHub {
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret${this.client_secret}`);
+
     const profile = await profileResponse.json();
 
     return {
